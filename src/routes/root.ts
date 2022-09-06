db103995f7d5c6c2ad14e07c465171b61cbdadd5
NickHafner <nickhafner@outlook.com>
2022-09-06 07:28:30 -0500

    end of line correction

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,1 +1,8 @@
-import { FastifyPluginAsync } from "fastify";const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {  fastify.get("/", async function (request, reply) {    return "this is an example";  });};export default example;
+import { FastifyPluginAsync } from "fastify";
+
+const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get("/", async function (request, reply) {
+    return "this is an example";
+  });
+};
+export default example;
